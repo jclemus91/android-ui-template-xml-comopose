a1ddd5ce515438d6c2d50cf68ca0e99a9305cf08
Julio Lemus <julio.lemus.lara@gmail.com>
2025-09-16 12:59:12 -0600

    Libraries
    -Compose
    -UI Legacy
    -Utils
    -Core
    
    Signed-off-by: Julio Lemus <julio.lemus.lara@gmail.com>

--- ui-app/app/build.gradle.kts
+++ ui-app/app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -37,20 +40,28 @@ android {
 }
 
 dependencies {
+    // -------- Compose --------
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose.core)
+    implementation(libs.bundles.compose.ui)
+    debugImplementation(libs.compose.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+    androidTestImplementation(libs.compose.ui.test.junit4)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    // -------- Views (legacy) --------
+    implementation(libs.bundles.views.core)
+
+    // -------- Utilities --------
+    implementation(libs.coil.compose)
+    implementation(libs.coil.core)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+    implementation(libs.kotlinx.serialization.json)
+
+    // -------- Test --------
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.rules)
 }
